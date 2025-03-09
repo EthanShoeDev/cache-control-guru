61e688c4a0ba37e4ddb9fe8dadcf7e34c1733876
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2025-03-09 03:14:07 -0400

    add analytics

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,7 @@
 // @refresh reload
 import { mount, StartClientTanstack } from '@solidjs/start/client';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 mount(() => <StartClientTanstack />, document.getElementById('app')!);
